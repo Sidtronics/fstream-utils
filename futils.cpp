d1e15f0f92544324df2c19dfad9a91db7e7dd318
Siddhesh Dharme <siddheshdharme18@gmail.com>
2022-08-24 10:31:01 +0530

    improved readBlock()

--- futils.cpp
+++ futils.cpp
@@ -11,17 +11,13 @@ ifile(_ifile)
 }
 
 std::string inFile::readBlock(std::string blkname){
-
-   std::string blkstring =  "###" + blkname + "###";
-   size_t blkbeg = fileContents.find(blkstring);
-
-   ifile->seekg(blkbeg);
-   ifile->ignore(blkstring.length());
-   blkbeg = ifile->tellg();
-
-   size_t blkend = fileContents.find(blkstring, blkbeg);
-
-   return fileContents.substr(blkbeg, blkend - blkbeg);
+    size_t initpos = ifile->tellg();
+    ifile->seekg(0);
+    std::string line, blk = "";
+    while(std::getline(*ifile, line)) if(line == "###" + blkname + "###") break;
+    while(std::getline(*ifile, line)) if(line == "###" + blkname + "###") break; else blk += line + '\n';
+    ifile->seekg(initpos);
+    return blk;
 }
 
 std::string inFile::getFileContents(){
